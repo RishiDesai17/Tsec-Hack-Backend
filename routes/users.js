90255f9284b1089becfdf55bde8e49804528b930
Rishi N Desai <51089917+RishiDesai17@users.noreply.github.com>
2020-02-12 18:35:46 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -7,14 +7,14 @@ router.post('/signup', UsersController.users_signup)
 
 router.post('/login',UsersController.users_login)
 
-router.get('/:userId',UsersController.get_user)
+router.get('/:userId',checkAuth,UsersController.get_user)
 
 router.delete('/:userId',checkAuth ,UsersController.users_delete_user)
 
-router.patch('/:userId',UsersController.update)
+router.patch('/:userId',checkAuth,UsersController.update)
 
 router.post('/token',UsersController.check)
 
-router.post('/message',UsersController.message)
+router.post('/message',checkAuth,UsersController.message)
 
 module.exports = router;
